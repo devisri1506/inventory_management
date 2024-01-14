3703a955d089bf8f1129dfe847cc352bbf4a5ed0
devisri1506 <91016987+devisri1506@users.noreply.github.com>
2024-01-14 15:52:42 +0530

    Add files via upload

---
+++ src/Components/login/Login.js
@@ -0,0 +1,116 @@
+import React, { useState, useEffect } from "react";
+import basestyle from "../Base.module.css";
+import loginstyle from "./Login.module.css";
+import { useNavigate, NavLink } from "react-router-dom";
+
+const Login = ({ setUserState }) => {
+  const navigate = useNavigate();
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
+  const [user, setUserDetails] = useState({
+    email: "",
+    password: "",
+  });
+
+  const changeHandler = (e) => {
+    const { name, value } = e.target;
+    setUserDetails({
+      ...user,
+      [name]: value,
+    });
+  };
+
+  const validateForm = (values) => {
+    const error = {};
+    const regex = /^[^\s+@]+@[^\s@]+\.[^\s@]{2,}$/i;
+    if (!values.email) {
+      error.email = "Email is required";
+    } else if (!regex.test(values.email)) {
+      error.email = "Please enter a valid email address";
+    }
+    if (!values.password) {
+      error.password = "Password is required";
+    }
+    return error;
+  };
+
+  const loginHandler = (e) => {
+    e.preventDefault();
+    setFormErrors(validateForm(user));
+    setIsSubmit(true);
+  };
+
+  useEffect(() => {
+    if (Object.keys(formErrors).length === 0 && isSubmit) {
+      // Simulate local validation
+      const Admin = { email: "Admin@gmail.com", password: "admin123" };
+      const employee={email:"employee@gmail.com",password:"employee123"};
+      const developer={email:"developer@gmail.com",password:"developer123"};
+
+      if (
+        user.email === Admin.email &&
+        user.password === Admin.password
+      ) {
+        // Simulate successful login
+        alert("Login successful!");
+        setUserState(Admin);
+        navigate("./././public/adminpage.html", { replace: true });
+      } 
+      else if (
+        user.email === employee.email &&
+        user.password === employee.password
+      ) {
+        // Simulate successful login
+        alert("Login successful!");
+        setUserState(employee);
+        navigate("./././public/employeepage.html", { replace: true });
+      } 
+      else if (
+        user.email === developer.email &&
+        user.password === developer.password
+      ) {
+        // Simulate successful login
+        alert("Login successful!");
+        setUserState(developer);
+        navigate("./././public/developerPage.html", { replace: true });
+      } 
+
+      else {
+        // Simulate failed login
+        alert("Invalid email or password. Please try again.");
+      }
+    }
+  }, [formErrors, isSubmit]);
+
+  return (
+    <div className={loginstyle.login}>
+      <form>
+        <h1>Login</h1>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Email"
+          onChange={changeHandler}
+          value={user.email}
+        />
+        <p className={basestyle.error}>{formErrors.email}</p>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Password"
+          onChange={changeHandler}
+          value={user.password}
+        />
+        <p className={basestyle.error}>{formErrors.password}</p>
+        <button className={basestyle.button_common} onClick={loginHandler}>
+          Login
+        </button>
+      </form>
+      <NavLink to="/signup">Not yet registered? Register Now</NavLink>
+    </div>
+  );
+};
+
+export default Login;
